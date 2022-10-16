6400057ed597798e4f72f63af3673859905d2f56
Zeriab Chiah <zeriab@hotmail.com>
2022-10-16 16:01:00 +0300

    auth account

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -55,7 +55,7 @@ export const myPassport = passport.use(
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
       callbackURL:
-        'https://zshopping-backend.herokuapp.com/api/v1/users/auth/google/callback',
+        'https://zshopping-backend.herokuapp.com/api/v1/users/auth/account',
       passReqToCallback: true,
     },
     function (
